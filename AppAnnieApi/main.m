4470d0a5b0df191d8072c5ff5f7febca07db02ec
Sam Oakley <sam@blork.co.uk>
2013-06-09 13:47:42 +0100

    Initial Commit

---
+++ AppAnnieApi/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  AppAnnieApi
+//
+//  Created by Sam Oakley on 09/06/2013.
+//  Copyright (c) 2013 Sam Oakley. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
